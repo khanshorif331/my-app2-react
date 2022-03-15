7d8e1769c74145a24ac298f571b71a61f61f18f9
khanshorif331 <khanshorif331@gmail.com>
2022-03-15 23:28:02 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+const number = 55555
+const singer = {name: 'Dr Mahfuz', job: 'singer'}
+const singer2 = {name: 'Eva Rahman', job: 'singer'}
+
+const singerStyle ={
+  color: 'purple',
+  backgroundColor: 'white'
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>JSX</h1>
+        <div className="container">
+          <h3>Hello Dude react how are you?</h3>
+        </div>
+        <div className="music">
+          <p>Name: {number}</p>
+          <p style={singerStyle}>Name: {singer.name} {singer.job}</p>
+          <p style={{color:'blue',backgroundColor:'yellow'}}>Name: {singer2.name} {singer2.job}</p>
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -13,7 +31,7 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noopener noreferrer" 
         >
           Learn React
         </a>
