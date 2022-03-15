42b326489012256e0de0a7f9e62b3ee97517418a
khanshorif331 <khanshorif331@gmail.com>
2022-03-15 23:56:11 +0600

    some components made

--- src/App.js
+++ src/App.js
@@ -13,31 +13,36 @@ const singerStyle ={
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>JSX</h1>
-        <div className="container">
-          <h3>Hello Dude react how are you?</h3>
-        </div>
-        <div className="music">
-          <p>Name: {number}</p>
-          <p style={singerStyle}>Name: {singer.name} {singer.job}</p>
-          <p style={{color:'blue',backgroundColor:'yellow'}}>Name: {singer2.name} {singer2.job}</p>
-        </div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer" 
-        >
-          Learn React
-        </a>
-      </header>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <h5>New Component!!!Yeee!!!</h5>
+      <Friend></Friend>
+      <Friend></Friend>
+      <Friend></Friend>
+      <Friend></Friend>
     </div>
   );
+}
+
+
+function Person(){
+  return (
+    <div className="person">
+      <h1>Sakib Al Hasan</h1>
+      <p>Profession: Cricket</p>
+    </div>
+  )
+}
+
+function Friend (){
+  return(
+    <div className='container'>
+      <h3>Name: Ajay Devgun</h3>
+      <p>Profession: Maramari</p>
+    </div>
+  )
 }
 
 export default App;
