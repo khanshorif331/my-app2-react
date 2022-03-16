a6b4b972a3ea8067dc6adbbe50085240bebbfa2b
khanshorif331 <khanshorif331@gmail.com>
2022-03-16 21:31:17 +0600

    state changing is made

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 const number = 55555
 const singers = [
@@ -14,33 +15,62 @@ const singerStyle ={
   backgroundColor: 'white'
 }
 
+
 function App() {
-  const nayoks = ['Rubel','Bapparaz','Kuber','Nayan','Pappi']
   
   return ( 
-    <div className="App">    
-      {
-        nayoks.map(nayok=><li>Name: {nayok}</li>)
-      }
-      {/* {
-        nayoks.map(nayok=><Person name={nayok}></Person>)
-      } */}
-      {
-        singers.map(singer=><Person name={singer.name}></Person>)
-      }
-      
-      <Person name="Prottoy Heron" age="40" profession="Actor"></Person>
-      <Person name="Saiful Sumon" age="23" profession="Student"></Person>
-      <Person name="Abdulla Al Erfan" age="35" profession="Manager"></Person>
-      <Person name="Mazharul Islam" age="31" profession="Trader"></Person>
-      <h5>New Component!!!Yeee!!!</h5>
-      <Friend></Friend>
-      <Friend></Friend>
-      <Friend></Friend>
-      <Friend></Friend>
+    <div className="App">
+      <Counter></Counter>
+
     </div>
   );
 }
+
+function Counter (){
+  const [count,setCount]=useState(20)
+  const increaseCount =()=>setCount(count+1);
+  const decreaseCount =()=>setCount(count-1);
+  // console.log(abc);
+  return (
+    <div>
+      <h1>Count:{count}</h1>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
+    </div>
+  )
+}
+
+
+
+
+
+// function App() {
+//   const nayoks = ['Rubel','Bapparaz','Kuber','Nayan','Pappi']
+  
+//   return ( 
+//     <div className="App">    
+//       {
+//         nayoks.map(nayok=><li>Name: {nayok}</li>)
+//       }
+//       {/* {
+//         nayoks.map(nayok=><Person name={nayok}></Person>)
+//       } */}
+//       {
+//         singers.map(singer=><Person name={singer.name} job={singer.job}></Person>)
+//       }
+      
+//       <Person name="Prottoy Heron" age="40" profession="Actor"></Person>
+//       <Person name="Saiful Sumon" age="23" profession="Student"></Person>
+//       <Person name="Abdulla Al Erfan" age="35" profession="Manager"></Person>
+//       <Person name="Mazharul Islam" age="31" profession="Trader"></Person>
+//       <h5>New Component!!!Yeee!!!</h5>
+//       <Friend></Friend>
+//       <Friend></Friend>
+//       <Friend></Friend>
+//       <Friend></Friend>
+//     </div>
+//   );
+// }
 
 
 function Person(props){
